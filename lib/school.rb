266b024542b45a6306c3a044b2c344758f397798
Ryan K Burns <ryankburns@gmail.com>
2020-10-02 18:33:36 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -15,7 +15,7 @@ class School
   def add_student(student_name, grade)
     @grade = grade
     @student_name = student_name
-    if @roster[grade] == false
+    if @roster.include?(grade) == false
       @roster[grade] = []
   end
   @roster[grade] << student_name
