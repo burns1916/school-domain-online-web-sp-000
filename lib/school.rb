42391f20b1c29ced0c0d7911040c927afb14dd7a
Ryan K Burns <ryankburns@gmail.com>
2020-10-02 18:19:14 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,8 +12,7 @@ class School
   end
 
   def add_student(student_name, grade)
-    @roster['grade'] = []
-    @roster['grade'] << student_name
+    @school[roster][grade] = []
   end
   @roster
 end
