86410e9f3d6f465482a9622020bbed994eaca660
Ryan K Burns <ryankburns@gmail.com>
2020-10-02 18:10:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
 # code here!
+class School
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def roster
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    roster[grade] = []
+  end
+  roster
+end
